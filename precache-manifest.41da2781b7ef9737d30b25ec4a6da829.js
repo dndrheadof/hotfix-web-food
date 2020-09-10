1a35165bcfafd1c01dd2d4b00a4c51f6764f5aba
buuz <uleryt@mail.ru>
2020-09-10 12:38:29 +0300

    Updates

---
+++ precache-manifest.41da2781b7ef9737d30b25ec4a6da829.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a386ebd1a02dfb9df66e93f758d9ca9",
+    "url": "/hotfix-web-food/index.html"
+  },
+  {
+    "revision": "87831890b7d3eda88712",
+    "url": "/hotfix-web-food/static/css/main.a1107254.chunk.css"
+  },
+  {
+    "revision": "c444f49aa3b7be58448d",
+    "url": "/hotfix-web-food/static/js/2.900d7bd7.chunk.js"
+  },
+  {
+    "revision": "41b66b6fd6eecba581340794bb1831be",
+    "url": "/hotfix-web-food/static/js/2.900d7bd7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87831890b7d3eda88712",
+    "url": "/hotfix-web-food/static/js/main.5db5089e.chunk.js"
+  },
+  {
+    "revision": "b22dda5ed653eb6a799b",
+    "url": "/hotfix-web-food/static/js/runtime-main.9a09f28a.js"
+  },
+  {
+    "revision": "dfc54362fc2b0f7b99a4038b87dd9e64",
+    "url": "/hotfix-web-food/static/media/1.dfc54362.png"
+  },
+  {
+    "revision": "2048975925bd8553d8aaa5e1d9d1ffb1",
+    "url": "/hotfix-web-food/static/media/2.20489759.png"
+  },
+  {
+    "revision": "dbf82c38432d45de3c1c99f13c8b5fee",
+    "url": "/hotfix-web-food/static/media/3.dbf82c38.png"
+  },
+  {
+    "revision": "e671d53961f44185ee61256a0e6f13a5",
+    "url": "/hotfix-web-food/static/media/4.e671d539.png"
+  },
+  {
+    "revision": "35bcd06768ffafae4a5721b8f0c4fd8f",
+    "url": "/hotfix-web-food/static/media/burger.35bcd067.png"
+  },
+  {
+    "revision": "06277842ccc3f7c2a37085189dacdcbc",
+    "url": "/hotfix-web-food/static/media/check-symbol.06277842.svg"
+  },
+  {
+    "revision": "751f84c25a8d1ff159f499cbb9aa5bb7",
+    "url": "/hotfix-web-food/static/media/edit.751f84c2.svg"
+  },
+  {
+    "revision": "4fd2ba47b4f59d0a4382a8d81aae85d6",
+    "url": "/hotfix-web-food/static/media/kfc.4fd2ba47.png"
+  },
+  {
+    "revision": "60464810e8cfcde7a20fc19284c38a0e",
+    "url": "/hotfix-web-food/static/media/mcdac.60464810.png"
+  },
+  {
+    "revision": "1198ed41690cc0974aa8baa0e95ebd1b",
+    "url": "/hotfix-web-food/static/media/refresh-button.1198ed41.svg"
+  },
+  {
+    "revision": "4eb05fdd9f3d81f2f81cac5847e25cc9",
+    "url": "/hotfix-web-food/static/media/sub.4eb05fdd.png"
+  }
+]);
